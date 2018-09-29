818d6ad33368f9a04c3f21bcdbe3f00973f6c1ce
Szymon Nowak <sznowak13@gmail.com>
2018-09-29 14:18:28 +0200

    Added samper route

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session, flash
 from os import urandom
+from uuid import uuid1
+from persistence import db_manager
 
 app = Flask(__name__)
 
@@ -7,16 +9,27 @@ app = Flask(__name__)
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'GET':
+        if 'entry_uuid' in session:
+            flash(f"Thanks for sampling, {session['name']}")
+            session.clear()
         return render_template('index.html')
     elif request.method == 'POST':
-        name = request.form['entry_name']
-        return redirect( url_for('sampler', name=name) )
+        session['name'] = request.form['entry_name']
+        session['entry_uuid'] = str(uuid1())
+        return redirect( url_for('sampler') )
 
 
-@app.route('/sampler/<name>')
-def sampler(name):
-    colors = ['red-ish', 'yellow-ish', 'green-ish', 'purple-ish', 'brown-ish', 'blue-ish', 'pink-ish', 'orange-ish']
-    return render_template('sampler.html', name=name, colors=colors)
+@app.route('/sampler', methods=['POST', 'GET'])
+def sampler():
+    if request.method == 'GET':
+        colors = ['red-ish', 'yellow-ish', 'green-ish', 'purple-ish', 'brown-ish', 'blue-ish', 'pink-ish', 'orange-ish']
+        return render_template('sampler.html', colors=colors)
+    elif request.method == 'POST':
+        data_entry = request.form.to_dict()
+        data_entry['id'] = session['entry_uuid']
+        print(data_entry)
+        db_manager.add_entry(entry=data_entry)
+        return redirect( url_for('sampler') )
 
 
 if __name__ == '__main__':
