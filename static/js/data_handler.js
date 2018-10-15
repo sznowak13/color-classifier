e757e2f427ca650a2a2c7771c1419096ec9cb90b
Szymon Nowak <sznowak13@gmail.com>
2018-10-15 21:39:05 +0200

    Added datahandler module
    It handles ajax requests to server.

---
+++ static/js/data_handler.js
@@ -0,0 +1,18 @@
+const dataHandler = {
+  addEntry: function(buttonLabel) {
+      fetch('/add-entry', {
+          method: "POST",
+          headers: {
+                "Content-Type": "application/json; charset=utf-8"
+            },
+            body: JSON.stringify({
+                red: sampler.colorSample.red,
+                green: sampler.colorSample.green,
+                blue: sampler.colorSample.blue,
+                color_label: buttonLabel
+            })
+      })
+          .then(response => response.json())
+          .then(data => console.log(data))
+  }
+};
