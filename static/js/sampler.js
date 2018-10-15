9bb60e8e6a0f2cc834ceee9207df69e43f5d025e
Szymon Nowak <sznowak13@gmail.com>
2018-10-15 21:36:41 +0200

    Modified sampler.js
    Started using jquery and ajax for adding entries to database.

--- static/js/sampler.js
+++ static/js/sampler.js
@@ -4,25 +4,30 @@ var sampler = {
         green: 0,
         blue: 0
     },
-    randomColor: function() {
+    init: function () {
+        let buttons = $('button').toArray();
+        buttons.forEach((button) => {
+            button.addEventListener('click', this.sendData)
+        });
+        sampler.randomColor();
+        sampler.showColor();
+    },
+    randomColor: function () {
         this.colorSample.red = parseInt(Math.random() * 256);
         this.colorSample.green = parseInt(Math.random() * 256);
         this.colorSample.blue = parseInt(Math.random() * 256);
-
-        document.getElementById('red').setAttribute('value', `${this.colorSample.red}`);
-        document.getElementById('green').setAttribute('value', `${this.colorSample.green}`);
-        document.getElementById('blue').setAttribute('value', `${this.colorSample.blue}`);
     },
-    showColor: function() {
-        let square = document.getElementById('color-square');
-        square.setAttribute('data-red', this.colorSample.red);
-        square.style.backgroundColor = `rgb(${this.colorSample.red},${this.colorSample.green},${this.colorSample.blue})`;
+    showColor: function () {
+        $('#color-square').css('background-color', `rgb(${this.colorSample.red},${this.colorSample.green},${this.colorSample.blue})`);
+    },
+    sendData: function (ev) {
+        let clickedButton = ev.target;
+        dataHandler.addEntry(clickedButton.value);
     }
-}
+};
 
 function main() {
-    sampler.randomColor();
-    sampler.showColor();
+    sampler.init()
 }
 
 main();
